11456985f48992d22b8c909ca0762bbf91ef9291
Webbuildermn <gerald.ryan40@yahoo.com>
2020-02-24 17:15:13 -0600

    Passed more car tests.

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function makePersonObject(id, name, email ) {
   console.log(person);
   return person;
 }
-makePersonObject(1,2,3);
+
 /**
  * ### Challenge `getName`
  * 
@@ -84,6 +84,7 @@ function makeSmartPerson(nameArg) {
   }
   return smartPerson;
 }
+
 
 
 
@@ -162,9 +163,34 @@ function getCarInfoByIndex(inventory, index) {
  * For example, if getLastCarInfo is invoked passing the inventory inside /data/inventory.js,
  * it will return `This is a Lincoln Town Car`.
 */
-function getLastCarInfo(/* code here */) {
-  /* code here */
+
+var inventory = [
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+  { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
+  { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+  { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
+  { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
+  { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
+  { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
+  { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
+  { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
+  { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
+  { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 }
+  /// ... Truncated
+]
+
+function getLastCarInfo(inventory) {
+
+  const carMake = inventory[inventory.length-1].car_make;
+  const carModel = inventory[inventory.length-1].car_model;
+  console.log(carModel)
+  return `This is a ${carMake} ${carModel}.`
 }
+
+getLastCarInfo(inventory)
 
 /**
  * ### Challenge `getCarInfoById`
